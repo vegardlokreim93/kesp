68e143ed169dd9bc8c63aec13e93cdd1e5915d0c
Vegard Lokreim <vegard.lokreim@icloud.com>
2015-05-13 21:24:41 +0200

    ikke noe spess

--- public/js/main.js
+++ public/js/main.js
@@ -3,13 +3,16 @@ $(document).ready(function() {
 
 
 //Function to hide/show hidden-menu
-    $( "#activate-hidden" ).click( function() {
+    $( "#activate-hidden" ).on("click",  function() {
             event.preventDefault();
             $("#hidden-menu").toggle();
             var height = $("#hidden-menu").outerHeight(); //Get the height of hidden menu, to adjust body
             
-            if ($('#hidden-menu').is(":hidden")) {
+            if ($('#hidden-menu').hidden()) {
+
                 $("#content").css('margin-top', '0px');
+                
+
             }else{
                 $("#content").css('margin-top', '-'+height+'px');
             }
